a6b4a04d146e523f883e30d2977971ac79c060f4
cclin <181250083@smail.nju.edu.cn>
2021-05-27 18:36:27 +0800

    update: Home UI design

--- data_visualize/src/pages/Home.jsx
+++ data_visualize/src/pages/Home.jsx
@@ -1,23 +1,32 @@
 import React from "react";
 import "./Home.css";
 import { Layout, Menu } from "antd";
+import { AlibabaOutlined } from "@ant-design/icons";
 
 const { Header, Content, Footer } = Layout;
 
 const Home = () => {
   return (
     <Layout style={{ height: "100vh" }} className="home">
-      <Header>
-        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["1"]}>
-          <Menu.Item key="1">nav 1</Menu.Item>
-          <Menu.Item key="2">nav 2</Menu.Item>
-          <Menu.Item key="3">nav 3</Menu.Item>
+      <AlibabaOutlined className="alilogo" />
+      <Header style={{ height: "50px" }}>
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={["1"]}
+          style={{ lineHeight: "50px" }}
+        >
+          <Menu.Item key="1">产品销量排名</Menu.Item>
+          <Menu.Item key="2">交易量走向</Menu.Item>
+          <Menu.Item key="3">热门产品销售浏览对比</Menu.Item>
         </Menu>
       </Header>
       <Content className="content">
         <div className="show"></div>
       </Content>
-      <Footer className="footer">Ant Design ©2018 Created by Ant UED</Footer>
+      <Footer className="footer">
+        @Ali Tianchi data vizualization web designed by cclin
+      </Footer>
     </Layout>
   );
 };
